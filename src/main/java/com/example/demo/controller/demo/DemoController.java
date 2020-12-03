b8bfd91a14a44a35bf52aa009290aebe08a658ee
Apisit <tabsolutet@gmail.com>
2020-12-03 23:33:03 +0700

    first commit

---
+++ src/main/java/com/example/demo/controller/demo/DemoController.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/demo")
+public class DemoController {
+
+    @GetMapping(path = "/getdemo")
+    public String getDemo() {
+        return "OK";
+    }
+
+}
